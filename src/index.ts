a2ba5e93850ccefd79b9a0b1b15a663bc850dca1
RafaelGondi <rdias.ga@gmail.com>
2025-10-17 14:28:20 -0300

    refactor: atualiza configuração para usar endpoint index
    - Renomeia endpoint de 'search' para 'index'
    - Renomeia tipo CitizenModelType para CitizenSelectModelType
    - Atualiza exports do index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as CitizenSelect } from './components/CitizenSelect.vue';
+export { default as CitizenTable } from './components/CitizenTable.vue';
 
 export { default as CitizenComponentsPlugin } from './plugin';
 export { default } from './plugin';
@@ -6,4 +7,4 @@ export { default } from './plugin';
 export type { CitizenComponentsConfig } from './config';
 
 export type { Citizen } from './services/citizen/citizen.types';
-export type { CitizenModelType, Nullable } from './types';
+export type { CitizenSelectModelType, Nullable } from './types';
