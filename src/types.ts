605309e9732d3eb83f27c83542e29ff374fc00b7
RafaelGondi <rdias.ga@gmail.com>
2025-10-20 11:47:10 -0300

    feat: adiciona tipos TableField e CustomTableField

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,13 @@
-export type { Citizen, Nullable } from './services/citizen/citizen.types';
-
 import type { Citizen } from './services/citizen/citizen.types';
+
+export type { Citizen, Nullable } from './services/citizen/citizen.types';
 export type CitizenSelectModelType = Citizen | Partial<Citizen> | string | null;
+
+export interface TableField {
+	key: keyof Citizen | 'actions';
+	label: string;
+}
+
+export interface CustomTableField extends TableField {
+	visible: boolean;
+}
