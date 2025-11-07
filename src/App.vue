9645bce3520dbeb2ca7bc50eb391837ace926f23
RafaelGondi <rdias.ga@gmail.com>
2025-11-07 15:04:41 -0300

    refactor: remove internalValue e usa model diretamente no CitizenSelect

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
 	<div>
+		<!-- <pre>{{ citizen }}</pre> -->
+		<CitizenSelect
+			v-model="citizen"
+			show-button
+		/>
 		<!-- <CdsGrid
 			:cols="2"
 			gap="32px"
@@ -25,7 +30,7 @@
 			</div>
 		</CdsGrid> -->
 
-		<CitizenTable
+		<!-- <CitizenTable
 			v-model="citizens"
 			selection-variant="blue"
 			allow-selection
@@ -38,7 +43,14 @@
 					@cds-click="logCustomButtonClick(slotData)"
 				/>
 			</template>
-		</CitizenTable>
+		</CitizenTable> -->
+		<CdsButton
+			class="mt-7"
+			secondary
+			@button-click="changeModel"
+		>
+			Change Model
+		</CdsButton>
 	</div>
 </template>
 
@@ -56,6 +68,38 @@ function logCustomButtonClick({ data, field, colIndex, rowIndex }) {
 	console.info('field: ', field);
 	console.info('colIndex: ', colIndex);
 	console.info('rowIndex: ', rowIndex);
+}
+
+function changeModel() {
+	console.log('no change model')
+	citizen.value = {
+		'id': '05954ccb-188b-4e56-bba8-53211d60e343',
+		'name': 'Dr. Monique Orn',
+		'gender': 'F',
+		'cpf_responsible': null,
+		'mother_name': 'Jaime Powlowski DDS',
+		'cpf': '56826137918',
+		'cns': '440541140933040',
+		'birth_date': '1946-10-02',
+		'phone': '38 4017-7733',
+		'cellphone': '83 7530-1849',
+		'email': 'Delta1@hotmail.com',
+		'address': {
+			'cep': '44443',
+			'street': 'Jakubowski Manors',
+			'number': '625',
+			'complement': 'Suite 152',
+			'neighborhood': 'Shanahan Glen',
+			'city': 'Albinborough',
+			'uf': 'NY'
+		},
+		'race': 'black',
+		'co_cidadao': 2852,
+		'is_dead': true,
+		'pregnant': false,
+		'identification_document': '040516263',
+		'issuing_agency': 'SSP'
+	}
 }
 </script>
 
