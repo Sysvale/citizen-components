d74c9c8f3ccbd9cbc095b8ba794a021b9e566aa7
Igor Vinicius <igorvinimec@gmail.com>
2025-11-24 14:49:55 -0300

    bugfix: Remove uso do attrs e define props manualmente

--- src/components/CitizenSelect.vue
+++ src/components/CitizenSelect.vue
@@ -10,15 +10,16 @@
 			:fluid
 		>
 			<CdsTextInput
-				v-bind="$attrs"
 				v-model.trim="searchString"
-				state="default"
 				:fluid
+				:label
+				:placeholder
+				:state
+				:error-message
 				:disabled="isLoading || disabled"
 				@keydown.enter="search"
 				@blur="isActive = false"
 			/>
-
 			<SelectDropdown
 				v-if="isActive"
 				v-model="model"
@@ -93,12 +94,20 @@ const props = withDefaults(
 		variant?: string;
 		optionsField?: keyof Citizen;
 		disabled?: boolean;
+		label?: string;
+		placeholder?: string;
+		state?: string;
+		errorMessage?: string;
 	}>(),
 	{
 		fluid: false,
 		variant: 'green',
 		optionsField: 'name',
 		disabled: false,
+		label: 'Usuário SUS',
+		placeholder: 'Selecione um usuário',
+		state: 'default',
+		errorMessage: '',
 	}
 );
 
@@ -135,6 +144,7 @@ const payload = computed(() => ({
 }));
 
 watch(model, () => {
+	console.log('model', model.value);
 	if (!model.value) {
 		searchString.value = '';
 	} else if (typeof model.value === 'string') {
