34c124217946aada56275ae99773e3ca4e4de9e1
LEMSantos <lu.casmou.ra177833@gmail.com>
2025-11-27 11:30:39 -0300

    feat: adiciona o componente para a section do card de visualização do resumo do cidadão

---
+++ src/components/InternalComponents/ResumeSection.vue
@@ -0,0 +1,49 @@
+<template>
+	<CdsFlexbox
+		gap="1"
+		direction="column"
+		class="resume-section"
+	>
+		<CdsDivider
+			:text="title"
+			inline
+			dimmed
+		/>
+
+		<CdsGrid
+			cols="minmax(25%, 200px) minmax(25%, 200px) minmax(25%, 200px)"
+			row-gap="3"
+			col-gap="6"
+		>
+			<CdsGridItem
+				v-for="item in items"
+				:key="item.label"
+				:col-span="item.fill ? 3 : 1"
+			>
+				<CdsText
+					as="body-2"
+					color="n-600"
+					font-weight="semibold"
+				>
+					{{ item.label }}:
+				</CdsText>
+				<CdsText
+					as="body-2"
+					color="n-600"
+				>
+					{{ item.value }}
+				</CdsText>
+			</CdsGridItem>
+		</CdsGrid>
+	</CdsFlexbox>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	title: string,
+	items: Array<{ label: string, value: Nullable<string>, fill?: boolean }>,
+}>()
+</script>
+
+<style lang="scss" scoped>
+</style>
