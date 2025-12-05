be32e4e0c9c4c06bbc20d089d7b8616ceb822467
Lucas <lucas.barbosa.nas@gmail.com>
2025-12-05 11:23:37 -0300

    test: Ajusta mock de cidadão para usar CPF e CNS válidos

--- src/services/citizen/citizen.factory.ts
+++ src/services/citizen/citizen.factory.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import type { Citizen } from './citizen.types';
 import { Citizen as CitizenModel } from '@/models/Citizen';
+import ufs from '@/constants/ufs';
 
 export const makeCitizen = (overrides?: Partial<Citizen>): CitizenModel => {
 	return new CitizenModel({
@@ -8,8 +9,8 @@ export const makeCitizen = (overrides?: Partial<Citizen>): CitizenModel => {
 		name: faker.person.fullName(),
 		gender: faker.helpers.arrayElement(['M', 'F']),
 		mother_name: faker.person.fullName(),
-		cpf: faker.number.int({ min: 10000000000, max: 99999999999 }).toString(),
-		cns: faker.string.numeric(15),
+		cpf: faker.helpers.arrayElement(['04320568664', '47388253807', '11930974442', '95151567500']),
+		cns: faker.helpers.arrayElement(['947401207820008', '150383531400002', '192134169240003', '146350365620000']),
 		birth_date: faker.date.birthdate().toISOString().split('T')[0] || '',
 		phone: faker.helpers.fromRegExp(/[0-9]{2} [0-9]{4}-[0-9]{4}/),
 		cellphone: faker.helpers.fromRegExp(/[0-9]{2} [0-9]{4}-[0-9]{4}/),
@@ -21,7 +22,7 @@ export const makeCitizen = (overrides?: Partial<Citizen>): CitizenModel => {
 			complement: faker.location.secondaryAddress(),
 			neighborhood: faker.location.street(),
 			city: faker.location.city(),
-			uf: faker.location.state({ abbreviated: true }),
+			uf: faker.helpers.arrayElement(ufs),
 		},
 		race: faker.helpers.arrayElement([
 			'white',
@@ -39,7 +40,7 @@ export const makeCitizen = (overrides?: Partial<Citizen>): CitizenModel => {
 	});
 };
 
-export const makeCitizens = (citizenCount = 1, overrides?: Partial<Citizen>): Citizen[] => {
+export const makeCitizens = (citizenCount = 1, overrides?: Partial<Citizen>): CitizenModel[] => {
 	faker.seed(123);
 	return Array.from({ length: citizenCount }, () => makeCitizen(overrides));
 };
